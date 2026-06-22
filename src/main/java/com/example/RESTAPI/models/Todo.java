ecde72c99ce035039dda55a4e5e5add8d152f7d6
Jeevankumar S <jeevenkumar2003@gmail.com>
2026-06-22 19:30:59 +0530

    learn: data validation

--- src/main/java/com/example/RESTAPI/models/Todo.java
+++ src/main/java/com/example/RESTAPI/models/Todo.java
@@ -4,7 +4,18 @@ package com.example.RESTAPI.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.Singular;
+import org.antlr.v4.runtime.misc.NotNull;
+
+//Lambok - getter and setter will be done automatically
+// Getter is used only to get the values
+// Setter is used only to set the values
 
 @Entity
 @Data
@@ -12,7 +23,15 @@ public class Todo {
     @Id
     @GeneratedValue
     Long id;
+    @NotBlank
+    @NonNull
+    @Size(min=2, max=15)
     String title;
+    @NotBlank
+    @NonNull
+    @Size(min=5, max=20)
     String description;
     Boolean isCompleted;
+    @Email
+    String email;
 }
